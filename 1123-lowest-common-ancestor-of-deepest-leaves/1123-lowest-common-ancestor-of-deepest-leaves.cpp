c2ea5da005223930751c651ee4122280552791ca
Samir Alam <sa8103339@gmail.com>
2026-08-16 10:57:25 +0530

    Time: 0 ms (100%), Space: 22 MB (64.45%) - LeetHub

--- 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.cpp
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.cpp
@@ -9,40 +9,73 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// my approach 
+
+// find maximum depth
+// store all nodes at that maximum depth
+// fidn the LCA of all those nodes
+
+
+
+
 class Solution {
-public:
-    unordered_map<int,int> mp;
-    int maxD = 0;
+    public:
+    int maxDepth(TreeNode* root){
+        if(root == NULL) return 0;
 
-    TreeNode* LCA(TreeNode* root){
-        if(root == NULL) return NULL;
-        if(mp[root->val] == maxD){
-            return root;
-        }
+        int left = maxDepth(root->left);
+        int right = maxDepth(root->right);
 
-        TreeNode* l = LCA(root->left);
-        TreeNode* r = LCA(root->right);
-
-        if(l != NULL && r != NULL){
-            return root;
-        }
-        if(l != NULL){
-            return l;
-        }
-        return r;
+        return 1 + max(left,right);
     }
 
-    void depth(TreeNode* root, int d){
+    // store all nodes having maxium depth
+    void findDeepest(TreeNode* root, int depth, int maxD, vector<TreeNode*> &arr) {
         if(root == NULL) return;
-        maxD = max(maxD, d);
-        mp[root->val] = d;
 
-        depth(root->left, d+1);
-        depth(root->right, d+1);
+        if(depth == maxD){
+            arr.push_back(root);
+            return;
+        }
+
+        findDeepest(root->left, depth+1, maxD, arr);
+        findDeepest(root->right, depth+1, maxD, arr);
     }
 
-    TreeNode* lcaDeepestLeaves(TreeNode* root) {
-        depth(root,0);
-        return LCA(root); 
+    TreeNode* LCA(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == NULL) return NULL;
+
+        if(root == p || root == q){
+            return root;
+        }
+
+        TreeNode* left = LCA(root->left, p, q);
+        TreeNode* right = LCA(root->right, p ,q);
+
+        if(left != NULL && right != NULL){
+            return root;
+        }
+        if(left != NULL){
+            return left;
+        }
+        return right;
+    }
+
+    TreeNode* lcaDeepestLeaves(TreeNode* root){
+        // find the maxium depth
+        int maxD = maxDepth(root);
+
+        // store deepest nodes
+        vector<TreeNode*> arr;
+
+        findDeepest(root,1, maxD, arr);
+
+        // fidn LCA of all deepest nodes
+        TreeNode* ans = arr[0];
+
+        for(int i=1; i<arr.size(); i++){
+            ans = LCA(root, ans, arr[i]);
+        }
+        return ans;
     }
 };
