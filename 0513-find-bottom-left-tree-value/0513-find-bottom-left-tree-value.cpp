51f94914d8260bc61b8626e252cec184d57e51e3
Samir Alam <sa8103339@gmail.com>
2026-08-21 21:48:10 +0530

    Time: 0 ms (100%), Space: 25.2 MB (27.22%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
@@ -11,23 +11,29 @@
  */
 class Solution {
 public:
-    int ans;
-    int maxDepth = -1;
-
-    void dfs(TreeNode* root, int depth){
-        if(root == NULL) return;
-
-        if(depth > maxDepth){
-            maxDepth = depth;
-            ans = root->val;
-        }
-
-        dfs(root->left, depth+1);
-        dfs(root->right, depth+1);
-
-    }
     int findBottomLeftValue(TreeNode* root) {
-        dfs(root, 0);
-        return ans;    
+        int ans;
+        queue<TreeNode*> que;
+        que.push(root);
+
+        while(!que.empty()){
+            int n = que.size();
+
+            while(n--){
+                TreeNode* node = que.front();
+                que.pop();
+
+                ans = node->val;
+
+                if(node->right){
+                    que.push(node->right);
+                }
+                if(node->left){
+                    que.push(node->left);
+                }
+            }
+        }
+        return ans;
+        
     }
 };
