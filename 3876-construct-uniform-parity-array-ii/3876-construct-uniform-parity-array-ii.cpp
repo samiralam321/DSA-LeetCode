1c7f538da7b2f2e261806aaf9cf7a84e641bf41b
Samir Alam <sa8103339@gmail.com>
2026-09-03 09:57:56 +0530

    Time: 4 ms (65.18%), Space: 165.9 MB (62.83%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -3,15 +3,14 @@ public:
     bool uniformArray(vector<int>& nums1) {
         int mini = *min_element(nums1.begin(), nums1.end());
 
-        if(mini%2 != 0){
-            return true;
-        }
+        if(mini & 1) return true;
 
         for(int x : nums1){
-            if(x % 2 != 0){
+            if(x & 1){
                 return false;
             }
         }
-        return true; 
+        return true;
+        
     }
 };
