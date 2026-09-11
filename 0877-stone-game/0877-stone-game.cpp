933ede8ecea182f3d8b64cc33bc3af5890b23f51
Samir Alam <sa8103339@gmail.com>
2026-09-11 15:54:36 +0530

    Time: 14 ms (16.12%), Space: 11.5 MB (30.43%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,7 +1,28 @@
 class Solution {
 public:
-    bool stoneGame(vector<int>& piles) {
-        return true;
-        
+    int n;
+    int t[501][501];
+
+    int solve(vector<int>& piles, int i, int j){
+        if(i > j) return 0;
+
+        if(t[i][j] != -1){
+            return t[i][j];
+        }
+
+        int choose_i = piles[i] + min(solve(piles, i+2, j), solve(piles, i+1, j-1));
+        int choose_j = piles[j] + min(solve(piles, i, j-2), solve(piles, i+1, j-1));
+
+        return t[i][j] = max(choose_i, choose_j);
+    }
+
+    bool stoneGame(vector<int>& piles){
+        n = piles.size();
+        memset(t, -1, sizeof(t));
+
+        int sum = accumulate(piles.begin(), piles.end(), 0);
+        int alice_score = solve(piles, 0, n-1);
+
+        return alice_score > sum/2;
     }
 };
